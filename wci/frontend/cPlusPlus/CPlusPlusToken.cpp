4d1706c1a5b9edae4d35877599d557be939af2da
RIYA JAIN <RIYA JAIN@10.250.219.188>
2018-09-07 18:44:58 -0700

    CPlusPlusToken.cpp Changed reserved words, symbols and identifiers

--- wci/frontend/cPlusPlus/CPlusPlusToken.cpp
+++ wci/frontend/cPlusPlus/CPlusPlusToken.cpp
@@ -27,54 +27,53 @@ void CPlusPlusToken::initialize()
 
     vector<string> rw_strings =
     {
-        "AND", "ARRAY", "BEGIN", "CASE", "CONST", "DIV", "DO", "DOWNTO",
-        "ELSE", "END", "FILE", "FOR", "FUNCTION", "GOTO", "IF", "IN",
-        "LABEL", "MOD", "NIL", "NOT", "OF", "OR", "PACKED", "PROCEDURE",
-        "PROGRAM", "RECORD", "REPEAT", "SET", "THEN", "TO", "TYPE",
-        "UNTIL", "VAR", "WHILE", "WITH"
+    		 "AUTO", "DOUBLE", "INT", "STRUCT", "BREAK", "ELSE", "LONG", "SWITCH",
+    		 "CASE", "ENUM", "NAMESPACE", "TEMPLATE", "CHAR", "EXTERN", "OPERATOR", "THIS",
+    		 "CLASS", "FLOAT", "PROTECTED", "THROW", "CONST", "FOR", "PUBLIC", "UNION",
+    		 "CONTINUE", "GOTO", "RETURN", "VOID", "DO", "IF", "STATIC",
+    		 "WHILE", "VAR", "WHILE"
     };
 
     vector<CPlusPlusTokenType> rw_keys =
     {
-        CPlusPlusTokenType::AND,
-        CPlusPlusTokenType::ARRAY,
-        CPlusPlusTokenType::BEGIN,
-        CPlusPlusTokenType::CASE,
-        CPlusPlusTokenType::CONST,
-        CPlusPlusTokenType::DIV,
-        CPlusPlusTokenType::DO,
-        CPlusPlusTokenType::DOWNTO,
+    		CPlusPlusTokenType::AUTO,
+    		        CPlusPlusTokenType::DOUBLE,
+    		        CPlusPlusTokenType::INT,
+    		        CPlusPlusTokenType::STRUCT,
+    		        CPlusPlusTokenType::BREAK,
+    		        CPlusPlusTokenType::ELSE,
+    		        CPlusPlusTokenType::LONG,
+    		        CPlusPlusTokenType::SWITCH,
 
-        CPlusPlusTokenType::ELSE,
-        CPlusPlusTokenType::END,
-        CPlusPlusTokenType::FILE,
-        CPlusPlusTokenType::FOR,
-        CPlusPlusTokenType::FUNCTION,
-        CPlusPlusTokenType::GOTO,
-        CPlusPlusTokenType::IF,
-        CPlusPlusTokenType::IN,
+    		        CPlusPlusTokenType::CASE,
+    		        CPlusPlusTokenType::ENUM,
+    		        CPlusPlusTokenType::NAMESPACE,
+    		        CPlusPlusTokenType::TEMPLATE,
+    		        CPlusPlusTokenType::CHAR,
+    		        CPlusPlusTokenType::EXTERN,
+    		        CPlusPlusTokenType::OPERATOR,
+    		        CPlusPlusTokenType::THIS,
 
-        CPlusPlusTokenType::LABEL,
-        CPlusPlusTokenType::MOD,
-        CPlusPlusTokenType::NIL,
-        CPlusPlusTokenType::NOT,
-        CPlusPlusTokenType::OF,
-        CPlusPlusTokenType::OR,
-        CPlusPlusTokenType::PACKED,
-        CPlusPlusTokenType::PROCEDURE,
+    		        CPlusPlusTokenType::CLASS,
+    		        CPlusPlusTokenType::FLOAT,
+    		        CPlusPlusTokenType::PROTECTED,
+    		        CPlusPlusTokenType::THROW,
+    		        CPlusPlusTokenType::CONST,
+    		        CPlusPlusTokenType::FOR,
+    		        CPlusPlusTokenType::PUBLIC,
+    		        CPlusPlusTokenType::UNION,
 
-        CPlusPlusTokenType::PROGRAM,
-        CPlusPlusTokenType::RECORD,
-        CPlusPlusTokenType::REPEAT,
-        CPlusPlusTokenType::SET,
-        CPlusPlusTokenType::THEN,
-        CPlusPlusTokenType::TO,
-        CPlusPlusTokenType::TYPE,
+    		        CPlusPlusTokenType::CONTINUE,
+    		        CPlusPlusTokenType::GOTO,
+    		        CPlusPlusTokenType::RETURN,
+    		        CPlusPlusTokenType::VOID,
+    		        CPlusPlusTokenType::DO,
+    		        CPlusPlusTokenType::IF,
+    		        CPlusPlusTokenType::STATIC,
 
-        CPlusPlusTokenType::UNTIL,
-        CPlusPlusTokenType::VAR,
-        CPlusPlusTokenType::WHILE,
-        CPlusPlusTokenType::WITH
+    		        CPlusPlusTokenType::WHILE,
+    		        CPlusPlusTokenType::VAR,
+    		        CPlusPlusTokenType::WHILE
     };
 
     for (int i = 0; i < rw_strings.size(); i++)
@@ -84,37 +83,65 @@ void CPlusPlusToken::initialize()
 
     vector<string> ss_strings =
     {
-        "+", "-", "*", "/", ":=", ".", ",", ";", ":", "'", "=", "<>",
-        "<", "<=", ">=", ">", "(", ")", "[", "]", "{", "}",  "^", ".."
+            "~", "!", "@", "%", "^", "&", "*", "-", "+", "=", "|", "/",
+            ":", ";", "?", "<", ">", ".", ",","'","\"","(",")","[","]", "{", "}",  "++", "--",
+    		"<<",">>","<=", ">=","+=","-=", "*=", "/=", "==","|=","%=","&=","^=", "!=","<<=",">>=", "||",
+    		"&&", "//", "/*", "*/"
     };
 
     vector<CPlusPlusTokenType> ss_keys =
     {
-        CPlusPlusTokenType::PLUS,
-        CPlusPlusTokenType::MINUS,
-        CPlusPlusTokenType::STAR,
-        CPlusPlusTokenType::SLASH,
-        CPlusPlusTokenType::COLON_EQUALS,
-        CPlusPlusTokenType::DOT,
-        CPlusPlusTokenType::COMMA,
-        CPlusPlusTokenType::SEMICOLON,
-        CPlusPlusTokenType::COLON,
-        CPlusPlusTokenType::QUOTE,
-        CPlusPlusTokenType::EQUALS,
-        CPlusPlusTokenType::NOT_EQUALS,
+    		 CPlusPlusTokenType::TILDE,
+    		        CPlusPlusTokenType::EXCLAMATION,
+    		        CPlusPlusTokenType::AT_SIGN,
+    		        CPlusPlusTokenType::PERCENTAGE,
+    		        CPlusPlusTokenType::CARET,
+    		        CPlusPlusTokenType::AND,
+    		        CPlusPlusTokenType::STAR,
+    		        CPlusPlusTokenType::MINUS,
+    		        CPlusPlusTokenType::PLUS,
+    		        CPlusPlusTokenType::EQUALS,
+    		        CPlusPlusTokenType::PIPE,
+    		        CPlusPlusTokenType::FRONT_SLASH,
 
-        CPlusPlusTokenType::LESS_THAN,
-        CPlusPlusTokenType::LESS_EQUALS,
-        CPlusPlusTokenType::GREATER_EQUALS,
-        CPlusPlusTokenType::GREATER_THAN,
-        CPlusPlusTokenType::LEFT_PAREN,
-        CPlusPlusTokenType::RIGHT_PAREN,
-        CPlusPlusTokenType::LEFT_BRACKET,
-        CPlusPlusTokenType::RIGHT_BRACKET,
-        CPlusPlusTokenType::LEFT_BRACE,
-        CPlusPlusTokenType::RIGHT_BRACE,
-        CPlusPlusTokenType::UP_ARROW,
-        CPlusPlusTokenType::DOT_DOT
+    		        CPlusPlusTokenType::COLON,
+    		        CPlusPlusTokenType::SEMICOLON,
+    		        CPlusPlusTokenType::QUESTION_MARK,
+    		        CPlusPlusTokenType::GREATER_THAN,
+    		        CPlusPlusTokenType::LESS_THAN,
+    		        CPlusPlusTokenType::PERIOD,
+    		        CPlusPlusTokenType::COMMA,
+    		        CPlusPlusTokenType::SINGLE_QUOTE,
+    		        CPlusPlusTokenType::DOUBLE_QUOTE,
+    		        CPlusPlusTokenType::LEFT_PAREN,
+    		        CPlusPlusTokenType::RIGHT_PAREN,
+    		        CPlusPlusTokenType::LEFT_BRACKET,
+    				CPlusPlusTokenType::RIGHT_BRACKET,
+    				CPlusPlusTokenType::LEFT_BRACE,
+    				CPlusPlusTokenType::RIGHT_BRACE,
+    				CPlusPlusTokenType::PLUS_PLUS,
+    				CPlusPlusTokenType::MINUS_MINUS,
+    				CPlusPlusTokenType::LEFT_SHIFT,
+    				CPlusPlusTokenTypE::RIGHT_SHIFT,
+    				CPlusPlusTokenType::LESS_EQUALS,
+    				CPlusPlusTokenType::GREATER_EQUALS,
+    				CPlusPlusTokenType::PLUS_EQUALS,
+    				CPlusPlusTokenType::MINUS_EQUALS,
+    				CPlusPlusTokenType::STAR_EQUALS,
+    				CPlusPlusTokenType::FRONT_SLASH_EQUALS,
+    				CPlusPlusTokenType::EQUALS_EQUALS,
+    				CPlusPlusTokenType::PIPE_EQUALS,
+    				CPlusPlusTokenType::PERCENTAGE_EQUALS,
+    				CPlusPlusTokenType::AND_EQUALS,
+    				CPlusPlusTokenType::CARET_EQUALS,
+    				CPlusPlusTokenType::EXCLAMATION_EQUALS,
+    				CPlusPlusTokenType::LEFT_SHIFT_EQUALS,
+    				CPlusPlusTokenType::RIGHT_SHIFT_EQUALS,
+    				CPlusPlusTokenType::PIPE_PIPE,
+    				CPlusPlusTokenType::AND_AND,
+    				CPlusPlusTokenType::FRONT_SLASH_FRONT_SLASH,
+    				CPlusPlusTokenType::FRONT_SLASH_STAR,
+    				CPlusPlusTokenType::STAR_FRONT_SLASH
     };
 
     for (int i = 0; i < ss_strings.size(); i++)
@@ -124,12 +151,17 @@ void CPlusPlusToken::initialize()
 
     vector<string> ss_names =
     {
-        "PLUS", "MINUS", "STAR", "SLASH", "COLON_EQUALS", "DOT", "COMMA",
-        "SEMICOLON", "COLON", "QUOTE", "EQUALS", "NOT_EQUALS",
-
-        "LESS_THAN", "LESS_EQUALS", "GREATER_EQUALS", "GREATER_THAN",
-        "LEFT_PAREN", "RIGHT_PAREN", "LEFT_BRACKET", "RIGHT_BRACKET",
-        "LEFT_BRACE", "RIGHT_BRACE", "UP_ARROW", "DOT_DOT"
+    		 "TILDE", "EXCLAMATION", "AT_SIGN", "PERCENTAGE", "CARET", "AND", "STAR",
+    		 "MINUS", "PLUS", "EQUALS", "PIPE", "FRONT_SLASH",
+    		 "COLON", "SEMICOLON", "QUESTION_MARK", "GREATER_THAN",
+    		 "LESS_THAN", "PERIOD", "COMMA", "SINGLE_QUOTE",
+    		 "DOUBLE_QUOTE", "LEFT_PAREN", "RIGHT_PAREN", "LEFT_BRACKET", "RIGHT_BRACKET",
+    		 "LEFT_BRACE","RIGHT_BRACE","PLUS_PLUS","MINUS_MINUS","LEFT_SHIFT","RIGHT_SHIFT","LESS_EQUALS", "GREATER_EQUALS", "PLUS_EQUALS",
+    		 "MINUS_EQUALS", "STAR_EQUALS", "FRONT_SLASH_EQUALS", "EQUALS_EQUALS",
+			 "PIPE_EQUALS", "PERCENTAGE_EQUALS", "AND_EQUALS", "CARET_EQUALS",
+			 "EXCLAMATION_EQUALS", "LEFT_SHIFT_EQUALS", "RIGHT_SHIFT_EQUALS",
+			 "PIPE_PIPE", "AND_AND", "FRONT_SLASH_FRONT_SLASH", "FRONT_SLASH_STAR",
+			 "STAR_FRONT_SLASH"
     };
 
     for (int i = 0; i < ss_names.size(); i++)
